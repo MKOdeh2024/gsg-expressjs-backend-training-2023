d7c790e88968a2ac8fa293ab89ecacd6ed622d45
Mohammad Khaled Mohammad Odah <137823341+MKOdeh2024@users.noreply.github.com>
2023-07-03 13:47:06 +0300

    Update script.js

--- learning-sprint-1/week3-task/script.js
+++ learning-sprint-1/week3-task/script.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+async function fetchData() {
+  try {
+    const response = await fetch('https://rickandmortyapi.com/api/character');
+    
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
